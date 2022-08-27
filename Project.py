21d1debd05963e83120347647ee21e2476229392
SalmaElhassika <69520963+SalmaElhassika@users.noreply.github.com>
2022-08-27 09:14:55 +0100

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,3 +1,8 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as matplot
+import seaborn as sns
+%matplotlib inline
+import streamlit as st
