dd7649339021549a88a769bac5e09081caa707d8
SalmaElhassika <69520963+SalmaElhassika@users.noreply.github.com>
2022-08-27 09:31:04 +0100

    Update Project.py

--- Project.py
+++ Project.py
@@ -4,7 +4,4 @@ import numpy as np
 
 st.markdown("# Main page 🎈")
 st.sidebar.markdown("# Main page 🎈")
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Page 2 ❄️")
-st.markdown("# Page 3 🎉")
-st.sidebar.markdown("# Page 3 🎉")
+
