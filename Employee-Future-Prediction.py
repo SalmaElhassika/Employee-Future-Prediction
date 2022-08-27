f45b80521316a72f9d5dbd4346857be12d09245e
SalmaElhassika <69520963+SalmaElhassika@users.noreply.github.com>
2022-08-27 09:59:51 +0100

    Update and rename Project.py to Employee-Future-Prediction.py

---
+++ Employee-Future-Prediction.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title('Employee-Future-Prediction')
+
+Employee = 'Employee-future-prediction'
+DATA_URL = ('https://www.kaggle.com/datasets/tejashvi14/employee-future-prediction')
